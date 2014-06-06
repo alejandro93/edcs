47549926a565d496f510d6df3675ab1040850369
mati <mati@PCI6-08.(none)>
2014-06-06 09:43:10 +0200

    Actualizacion

--- PGtkArray/PGtkArray/MainWindow.cs
+++ PGtkArray/PGtkArray/MainWindow.cs
@@ -5,23 +5,23 @@ public partial class MainWindow: Gtk.Window
 {	
 	public MainWindow (): base (Gtk.WindowType.Toplevel)
 	{
-		Build ();
-		
-		Gdk.Color greenColor = new Gdk.Color(0,255,0);
-		
-		int count = arrayButton.Rows * arrayButton.Columns;
-		
-		for (int index = 0; index < count; index++){
-			arrayButton.Get (index).Label = string.Format ("{0}", index+1);
-			if((index+1)%2 == 0)
-				arrayButton[index].ModifyBg(StateType.Normal, greenColor);
-		}
-		
-		arrayButton.Get(2,4).Label = "*";
-		arrayButton[3,5].Label = "*";
-		string[] Labels = {"Uno", "Dos", "Tres", "Cuatro"};
-		arrayButton.SetLabels(Labels);
-		
+//		Build ();
+//		
+//		Gdk.Color greenColor = new Gdk.Color(0,255,0);
+//		
+//		int count = arrayButton.Rows * arrayButton.Columns;
+//		
+//		for (int index = 0; index < count; index++){
+//			arrayButton.Get (index).Label = string.Format ("{0}", index+1);
+//			if((index+1)%2 == 0)
+//				arrayButton[index].ModifyBg(StateType.Normal, greenColor);
+//		}
+//		
+//		arrayButton.Get(2,4).Label = "*";
+//		arrayButton[3,5].Label = "*";
+//		string[] Labels = {"Uno", "Dos", "Tres", "Cuatro"};
+//		arrayButton.SetLabels(Labels);
+//		
 //		Table table = (Table)arrayButton.Child;
 //		Console.WriteLine("diseñador: table.Children.Length={0}", table.Children.Length);
 //		
